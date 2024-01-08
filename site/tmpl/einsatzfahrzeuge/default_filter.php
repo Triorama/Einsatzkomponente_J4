3cb7470ac2402c4a5eb921794e307dfeffecdee1
Triorama <65812753+Triorama@users.noreply.github.com>
2024-01-08 22:20:25 +0100

    reformatting

--- site/tmpl/einsatzfahrzeuge/default_filter.php
+++ site/tmpl/einsatzfahrzeuge/default_filter.php
@@ -9,6 +9,7 @@
  */
 
 defined('JPATH_BASE') or die;
+
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
@@ -21,30 +22,25 @@ $data['options'] = !empty($data['options']) ? $data['options'] : array();
 // Check if any filter field has been filled
 $filters = false;
 
-if (isset($data['view']->filterForm))
-{
+if (isset($data['view']->filterForm)) {
 	$filters = $data['view']->filterForm->getGroup('filter');
 }
 
 // Check if there are filters set.
-if ($filters !== false)
-{
+if ($filters !== false) {
 	$filterFields = array_keys($filters);
 	$filtered     = false;
 	$filled       = false;
 
-	foreach ($filterFields as $filterField)
-	{
+	foreach ($filterFields as $filterField) {
 		$filterField = substr($filterField, 7);
 		$filter      = $data['view']->getState('filter.' . $filterField);
 
-		if (!empty($filter))
-		{
+		if (!empty($filter)) {
 			$filled = $filter;
 		}
 
-		if (!empty($filled))
-		{
+		if (!empty($filled)) {
 			$filtered = true;
 			break;
 		}
@@ -66,35 +62,30 @@ $data['options'] = array_unique(array_merge($customOptions, $data['options']));
 $formSelector = !empty($data['options']['formSelector']) ? $data['options']['formSelector'] : '#adminForm';
 
 // Load search tools
-JHtml::_('searchtools.form', $formSelector, $data['options']);
+HTMLHelper::_('searchtools.form', $formSelector, $data['options']);
 ?>
 
 <div class="js-stools clearfix">
 	<div class="clearfix">
 		<div class="js-stools-container-bar">
-			<label for="filter_search" class="element-invisible"
-				   aria-invalid="false"><?php echo Text::_('COM_EINSATZKOMPONENTE_SEARCH_FILTER_SUBMIT'); ?></label>
+			<label for="filter_search" class="element-invisible" aria-invalid="false"><?php echo Text::_('COM_EINSATZKOMPONENTE_SEARCH_FILTER_SUBMIT'); ?></label>
 
 			<div class="btn-wrapper input-append">
 				<?php echo $filters['filter_search']->input; ?>
-				<button type="submit" class="btn hasTooltip" title=""
-						data-original-title="<?php echo Text::_('COM_EINSATZKOMPONENTE_SEARCH_FILTER_SUBMIT'); ?>">
+				<button type="submit" class="btn hasTooltip" title="" data-original-title="<?php echo Text::_('COM_EINSATZKOMPONENTE_SEARCH_FILTER_SUBMIT'); ?>">
 					<i class="icon-search"></i>
 				</button>
 			</div>
-			<?php if ($filters): ?>
+			<?php if ($filters) : ?>
 				<div class="btn-wrapper hidden-phone">
-					<button type="button" class="btn hasTooltip js-stools-btn-filter" title=""
-							data-original-title="<?php echo Text::_('COM_EINSATZKOMPONENTE_SEARCH_TOOLS_DESC'); ?>">
+					<button type="button" class="btn hasTooltip js-stools-btn-filter" title="" data-original-title="<?php echo Text::_('COM_EINSATZKOMPONENTE_SEARCH_TOOLS_DESC'); ?>">
 						<?php echo Text::_('COM_EINSATZKOMPONENTE_SEARCH_TOOLS'); ?> <i class="caret"></i>
 					</button>
 				</div>
 			<?php endif; ?>
 
 			<div class="btn-wrapper">
-				<button type="button" class="btn hasTooltip js-stools-btn-clear" title=""
-						data-original-title="<?php echo Text::_('COM_EINSATZKOMPONENTE_SEARCH_FILTER_CLEAR'); ?>"
-						onclick="jQuery(this).closest('form').find('input').val('');">
+				<button type="button" class="btn hasTooltip js-stools-btn-clear" title="" data-original-title="<?php echo Text::_('COM_EINSATZKOMPONENTE_SEARCH_FILTER_CLEAR'); ?>" onclick="jQuery(this).closest('form').find('input').val('');">
 					<?php echo Text::_('COM_EINSATZKOMPONENTE_SEARCH_FILTER_CLEAR'); ?>
 				</button>
 			</div>
@@ -102,7 +93,8 @@ JHtml::_('searchtools.form', $formSelector, $data['options']);
 	</div>
 	<!-- Filters div -->
 	<div class="js-stools-container-filters hidden-phone clearfix" style="">
-		<?php // Load the form filters ?>
+		<?php // Load the form filters 
+		?>
 		<?php if ($filters) : ?>
 			<?php foreach ($filters as $fieldName => $field) : ?>
 				<?php if ($fieldName != 'filter_search') : ?>
